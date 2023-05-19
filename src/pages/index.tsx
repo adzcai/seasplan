31f09418430e25556ca31c5ab21b944d782e98cb
Alexander Cai <alexandercai@outlook.com>
2023-05-18 21:49:40 -0600

    prototype

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,109 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Fragment, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const equals = (s: any) => (ss: any) => ss.calendarYear === s.calendarYear && ss.term === s.term;
+
+export default function Home({
+  semesters, meta, plan
+}: any) {
+  const [selectedSemesters, setSelectedSemesters] = useState(semesters);
+  const [selectedSubjects, setSelectedSubjects] = useState<string[]>(meta.catalogPrefixes);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className={"flex min-h-screen flex-col items-center p-24 " + inter.className}>
+      <h1>Filter by semester</h1>
+      <div className="flex flex-row">
+        {semesters.map((s: any) => (
+          <button
+          key={s}
+          className={"flex flex-col items-center justify-center p-4 rounded hover:bg-gray-500" + (
+            selectedSemesters.find(equals(s)) ? " bg-blue-300" : " bg-gray-300"
+          )}
+          onClick={() => {
+            if (selectedSemesters.find(equals(s))) {
+              setSelectedSemesters(selectedSemesters.filter((ss: any) => !equals(s)(ss)));
+            } else {
+              setSelectedSemesters([...selectedSemesters, s]);
+            }
+          }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            <h2>{s.calendarYear}</h2>
+            <h3>{s.term}</h3>
+          </button>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <h1>Subjects</h1>
+      <div className="flex flex-row">
+        {meta.catalogPrefixes.map((s: string) => (
+          <button
+          key={s}
+          className={"flex flex-col items-center justify-center p-4 rounded hover:bg-gray-500" + (
+            selectedSubjects.includes(s) ? " bg-blue-300" : " bg-gray-300"
+          )}
+          onClick={() => {
+            if (selectedSubjects.includes(s)) {
+              setSelectedSubjects(selectedSubjects.filter((ss: string) => ss !== s));
+            } else {
+              setSelectedSubjects([...selectedSubjects, s]);
+            }
+          }}
+          >
+            <h2>{s}</h2>
+          </button>
+        ))}
       </div>
+      <h1>Courses</h1>
+      <div className="grid grid-cols-2">
+        {plan
+        .filter((c: any) => {
+          const semesterMatch = c.semesters.find((s: any) => selectedSemesters.find(equals(s)))
+          const subjectMatch = selectedSubjects.includes(c.catalogPrefix);
+          return semesterMatch && subjectMatch;
+        })
+        .map((c: any) => (
+          <Fragment key={c.id}>
+            <h2>{c.catalogNumber}</h2>
+            <h3>{c.title}</h3>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+              {/* <h3>{c.id}</h3> */}
+            {/* <div className="flex flex-col">
+              {c.semesters.filter((s: any) => selectedSemesters.find(equals(s))).map((s: any) => (
+                <div key={s.calendarYear + s.term} className="flex flex-row">
+                  <h3>{s.calendarYear}</h3>
+                  <h4>{s.term}</h4>
+                </div>
+              ))}
+            </div> */}
+          </Fragment>
+        ))}
       </div>
     </main>
   )
+}
+
+export async function getServerSideProps() {
+  const metaRes = await fetch('https://computingapps.seas.harvard.edu/course-planner/api/metadata');
+  const planRes = await fetch('https://computingapps.seas.harvard.edu/course-planner/api/course-instances/multi-year-plan');
+  const meta = await metaRes.json();
+  const plan = await planRes.json();
+  const semesters: any[] = [];
+  for (const c of plan) {
+    for (const s of c.semesters) {
+      if (!semesters.find(equals(s))) {
+        const { calendarYear, term } = s;
+        semesters.push({ calendarYear, term });
+      }
+    }
+  }
+
+  return {
+    props: {
+      semesters,
+      meta,
+      plan
+    }
+  };
 }
